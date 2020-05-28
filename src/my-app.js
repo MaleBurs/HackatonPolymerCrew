05a782335a2176942e736c2dc225673c1bbfb1c0
igniparra <igniparra@gmail.com>
2020-05-28 13:53:26 -0300

    Update my-app.js

--- src/my-app.js
+++ src/my-app.js
@@ -8,36 +8,37 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
-import '@polymer/app-layout/app-drawer/app-drawer.js';
-import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
-import '@polymer/app-layout/app-header/app-header.js';
-import '@polymer/app-layout/app-header-layout/app-header-layout.js';
-import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
-import '@polymer/app-layout/app-toolbar/app-toolbar.js';
-import '@polymer/app-route/app-location.js';
-import '@polymer/app-route/app-route.js';
-import '@polymer/iron-pages/iron-pages.js';
-import '@polymer/iron-selector/iron-selector.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import '@polymer/paper-button/paper-button.js';
-import '@polymer/paper-input/paper-input.js';
-import '@polymer/iron-ajax/iron-ajax.js';
-import './my-icons.js';
+ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+ import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
+ import '@polymer/app-layout/app-drawer/app-drawer.js';
+ import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
+ import '@polymer/app-layout/app-header/app-header.js';
+ import '@polymer/app-layout/app-header-layout/app-header-layout.js';
+ import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+ import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+ import '@polymer/app-route/app-location.js';
+ import '@polymer/app-route/app-route.js';
+ import '@polymer/iron-pages/iron-pages.js';
+ import '@polymer/iron-selector/iron-selector.js';
+ import '@polymer/paper-icon-button/paper-icon-button.js';
+ import '@polymer/paper-button/paper-button.js';
+ import '@polymer/paper-input/paper-input.js';
+ import '@polymer/iron-ajax/iron-ajax.js';
+ import './my-icons.js';
+ import './shared-styles.js';
+ // Gesture events like tap and track generated from touch will not be
+ // preventable, allowing for better scrolling performance.
+ setPassiveTouchGestures(true);
 
-// Gesture events like tap and track generated from touch will not be
-// preventable, allowing for better scrolling performance.
-setPassiveTouchGestures(true);
+ // Set Polymer's root path to the same value we passed to our service worker
+ // in `index.html`.
+ setRootPath(MyAppGlobals.rootPath);
 
-// Set Polymer's root path to the same value we passed to our service worker
-// in `index.html`.
-setRootPath(MyAppGlobals.rootPath);
-
-class MyApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
+ class MyApp extends PolymerElement {
+   static get template() {
+     return html`
+       <style include="shared-styles">
+       <style>
         :host {
           --app-primary-color: #4285f4;
           --app-secondary-color: black;
@@ -61,7 +62,7 @@ class MyApp extends PolymerElement {
 
           text-transform: uppercase;
           letter-spacing: 1.7px;
-        
+
         }
 
         app-header paper-icon-button {
@@ -90,117 +91,108 @@ class MyApp extends PolymerElement {
         }
       </style>
 
-      <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
-      </app-location>
+       <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
+       </app-location>
 
-      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
-      </app-route>
+       <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+       </app-route>
 
-      <app-drawer-layout fullbleed="" narrow="{{narrow}}">
-        <!-- Drawer content -->
-        <template is=dom-if if='{{isLoggedIn}}'>
-        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="view1" href="[[rootPath]]view1">3Deseos</a>
-            <a name="login" href="[[rootPath]]login">Regalar</a>
-            <a name="view3" href="[[rootPath]]view3">Mi Perfil</a>
-          </iron-selector>
-        </app-drawer>
-        </template>
+       <app-drawer-layout fullbleed="" narrow="{{narrow}}">
+         <!-- Drawer content -->
+         <template is=dom-if if='{{isLoggedIn}}'>
+         <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+           <app-toolbar class="menu">Menu</app-toolbar>
+           <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+             <a class="textMenu" name="view1" href="[[rootPath]]view1">3Deseos</a>
+             <a class="textMenu" name="login" href="[[rootPath]]login">Regalar</a>
+             <a class="textMenu" name="view3" href="[[rootPath]]view3">Mi Perfil</a>
+           </iron-selector>
+           <img class="fondoDeTres" src="./images/background.png">
+         </app-drawer>
+         </template>
 
-        <template is=dom-if if='{{!isLoggedIn}}'>
-        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="view1" href="[[rootPath]]view1">3Deseos</a>
-            <a name="login" href="[[rootPath]]login">Ingresar</a>
-          </iron-selector>
-        </app-drawer>
-        </template>
+         <template is=dom-if if='{{!isLoggedIn}}'>
+         <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+           <app-toolbar class="menu">Menu</app-toolbar>
+           <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+             <a class="textMenu" name="view1" href="[[rootPath]]view1">3Deseos</a>
+             <a class="textMenu" name="login" href="[[rootPath]]login">Ingresar</a>
+           </iron-selector>
+           <img class="fondoDeTres" src="./images/background.png">
+         </app-drawer>
+         </template>
 
-        <!-- Main content -->
-        <app-header-layout has-scrolling-region="">
+         <!-- Main content -->
+         <app-header-layout has-scrolling-region="">
 
-          <app-header class="headerBar" slot="header" condenses="" reveals="" effects="waterfall">
-            <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">3Deseos</div>
-            </app-toolbar>
-          </app-header>
+           <app-header class="headerBar" slot="header" condenses="" reveals="" effects="waterfall">
+             <app-toolbar>
+               <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+               <img src="./images/Logo3Deseos.jpg" height="40px;" alt="3Deseos">
+               <div class="borderDivided">
+                 <div id="purple"></div>
+                 <div id="orange2"></div>
+                 <div id="purple2"></div>
+                 <div id="orange"></div>
+               </div>
+             </app-toolbar>
+           </app-header>
 
-          <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
-            <my-view1 name="view1"></my-view1>
-            <my-login name="login"></my-login>
-            <my-view3 name="view3"></my-view3>
-            <my-view404 name="view404"></my-view404>
-          </iron-pages>
-        </app-header-layout>
-      </app-drawer-layout>
-    `;
-  }
+           <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+             <my-view1 name="view1"></my-view1>
+             <my-login name="login"></my-login>
+             <my-view3 name="view3"></my-view3>
+             <my-view404 name="view404"></my-view404>
+           </iron-pages>
+         </app-header-layout>
+       </app-drawer-layout>
+     `;
+   }
 
-  static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true,
-        observer: '_pageChanged'
-      },
-      routeData: Object,
-      subroute: Object,
-      isLoggedIn: {
-        type: Boolean,
-        value: false
-      }
-    };
-  }
+   static get properties() {
+     return {
+       page: {
+         type: String,
+         reflectToAttribute: true,
+         observer: '_pageChanged'
+       },
+       routeData: Object,
+       subroute: Object,
+       isLoggedIn: {
+         type: Boolean,
+         value: false
+       }
+     };
+   }
 
-  static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ];
-  }
+   static get observers() {
+     return [
+       '_routePageChanged(routeData.page)'
+     ];
+   }
 
-  _routePageChanged(page) {
-     // Show the corresponding page according to the route.
+   _routePageChanged(page) {
+      // Show the corresponding page according to the route.
+      //
+      // If no page was found in the route data, page will be an empty string.
+      // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+     if (!page) {
+       this.page = 'view1';
+     } else if (['view1', 'login', 'view3'].indexOf(page) !== -1) {
+       this.page = page;
+     } else {
+       this.page = 'view404';
+     }
+
+     // Close a non-persistent drawer when the page & route are changed.
+     if (!this.$.drawer.persistent) {
+       this.$.drawer.close();
+     }
+   }
+
+   _pageChanged(page) {
+     // Import the page component on demand.
      //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
-    if (!page) {
-      this.page = 'view1';
-    } else if (['view1', 'login', 'view3'].indexOf(page) !== -1) {
-      this.page = page;
-    } else {
-      this.page = 'view404';
-    }
-
-    // Close a non-persistent drawer when the page & route are changed.
-    if (!this.$.drawer.persistent) {
-      this.$.drawer.close();
-    }
-  }
-
-  _pageChanged(page) {
-    // Import the page component on demand.
-    //
-    // Note: `polymer build` doesn't like string concatenation in the import
-    // statement, so break it up.
-    switch (page) {
-      case 'view1':
-        import('./my-view1.js');
-        break;
-      case 'login':
-        import('./login.js');
-        break;
-      case 'view3':
-        import('./my-view3.js');
-        break;
-      case 'view404':
-        import('./my-view404.js');
-        break;
-    }
-  }
      // Note: `polymer build` doesn't like string concatenation in the import
      // statement, so break it up.
      switch (page) {
